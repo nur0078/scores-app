7a0dbb9986a5d6d6b447b7bd59ebd0527494ede1
Anurodh <anurodhp7@gmail.com>
2024-02-27 00:05:00 +1100

    test data

---
+++ src/constants/index.js
@@ -0,0 +1,36 @@
+import manUtd from "../../src/assets/images/manUtd.png"
+
+export const fixtures = [
+    {
+      home: "Man United", 
+      homeLogo: manUtd,
+      away: "Man City", 
+      awayLogo: manUtd,
+      kickOff: "17:00",
+      stadium: "Old Trafford",
+    },
+    {
+      home: "Tottenham", 
+      homeLogo: manUtd,
+      away: "Newcastle", 
+      awayLogo: manUtd,
+      kickOff: "17:00",
+      stadium: "St. James' Park",
+    },
+    {
+      home: "Chelsea", 
+      homeLogo: manUtd,
+      away: "Arsenal", 
+      awayLogo: manUtd,
+      kickOff: "19:00",
+      stadium: "Stamford Bridge",
+    },
+    {
+      home: "Aston Villa", 
+      homeLogo: manUtd,
+      away: "West Ham", 
+      awayLogo: manUtd,
+      kickOff: "19:00",
+      stadium: "Villa Park",
+    },
+  ];
