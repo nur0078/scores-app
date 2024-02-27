a105c931f070b48de9f1609b9eecc9d1967ba547
Anurodh <anurodhp7@gmail.com>
2024-02-28 00:13:44 +1100

    more padding to billboard

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div>
       <div id="home" className=" p-6  font-poppins md:h-lvh md:grid md:grid-cols-2 gap-10 justify-center items-center ">
-        <div id="billboard" className="w-[400px] pb-16 pr-10 md:pr-20 lg:w-[600px]  mx-auto">
+        <div id="billboard" className="w-[400px] pb-16 pr-16 md:pr-20 lg:w-[600px]  mx-auto">
             <Billboard />
         </div>
         <div id="fixtures" className="flex justify-center items-center  pb-14">
