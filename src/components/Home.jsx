ea32889e59957af68d478539811fcc1c8a1faa6d
Anurodh <anurodhp7@gmail.com>
2024-02-27 23:55:05 +1100

    add Standings, design all components responsively

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,23 @@
 import Billboard from "./Billboard"
+import Standings from "./Standings"
 import FixturesList from "./FixturesList"
 
 const Home = () => {
   return (
-    <div id="home" className="px-1 font-poppins">
-        <div id="billboard" className="mb-6">
+    <div>
+      <div id="home" className=" p-6  font-poppins md:h-lvh md:grid md:grid-cols-2 gap-10 justify-center items-center ">
+        <div id="billboard" className=" w-[400px] pb-16 md:pr-20 lg:w-[600px]  mx-auto">
             <Billboard />
         </div>
-        <div id="fixtures" className="">
+        <div id="fixtures" className="flex justify-center items-center  pb-14">
             <FixturesList />
         </div>
+      </div>
+      <div className="flex justify-center items-center">
+        <Standings />
+      </div>
     </div>
+
   )
 }
 
