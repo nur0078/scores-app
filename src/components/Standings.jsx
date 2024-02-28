5db3553a0527daf240ebcb1c0c6c6d9e07507aad
Anurodh <anurodhp7@gmail.com>
2024-02-28 17:11:52 +1100

    change table design and design logo adjustment

--- src/components/Standings.jsx
+++ src/components/Standings.jsx
@@ -1,65 +1,22 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { getTeamNickname, qualifiedLeague } from '../constants'
-
-// Dynamic league season
-var currentDate = new Date();
-// Get the current year
-var currentYear = currentDate.getFullYear();
-
-// Row color for relegation
-const isRelegated = (rank) => {
-  let setStyle = "justify-between pb-4 grid grid-cols-9 md:divide-y md:divide-gray-800  font-inter text-md "
-
-  if (rank > 17) {
-    setStyle = setStyle + "bg-red-300"
-    
-  } else if (rank < 5 ) {
-    setStyle = setStyle + "bg-green-200"
-  } else if (rank < 7) {
-    setStyle = setStyle + 'bg-orange-100'
-
-  } else {
-    setStyle = setStyle + "bg-slate-100"
-  }
-  return setStyle
-}
-
-//Qualified League logo
-const isLogoNeeded = (rank) => {
-    if (rank < 6 && rank < 18) {
-    return (
-      <span className="w-8 h-8 pl-2 pt-1"> 
-        <img
-          src={qualifiedLeague(rank)}
-          className="invisible md:visible"
-          alt="cup logo" 
-          width='15px'
-          height='15px'
-          />
-      </span> 
-    )
-  }
-}
-
-
+import { getTeamNickname, qualifiedLeague } from "../constants";
 
 const Standings = () => {
   const [tableData, setTableData] = useState([]);
-  const [userWidth, setUserWidth] = useState(0)
+  const [userWidth, setUserWidth] = useState(window.innerWidth);
 
   useEffect(() => {
     const handleResize = () => {
       setUserWidth(window.innerWidth);
     };
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
-
 
   useEffect(() => {
     const fetchTable = async () => {
@@ -68,7 +25,7 @@ const Standings = () => {
           "https://api-football-v1.p.rapidapi.com/v3/standings",
           {
             params: {
-              season: (currentYear-1) ? (currentYear-1).toString() : (currentYear-2).toString(),
+              season: (new Date().getFullYear() - 1).toString(),
               league: "39",
             },
             headers: {
@@ -79,48 +36,94 @@ const Standings = () => {
         );
         setTableData(response.data.response[0].league.standings[0]);
       } catch (error) {
-        console.error(error);
+        console.error("Error fetching table data:", error);
       }
     };
     fetchTable();
   }, []);
 
+  const isRelegated = (rank) => {
+    let setStyle = "border-b-2 border-gray-200 text-center font-inter text-sm ";
+
+    if (rank > 17) {
+      setStyle += "bg-red-100 text-red-700";
+    } else if (rank < 5) {
+      setStyle += "bg-green-100 text-green-700";
+    } else if (rank < 7) {
+      setStyle += "bg-yellow-100 text-yellow-700";
+    } else {
+      setStyle += "bg-gray-100 text-gray-700";
+    }
+    return setStyle;
+  };
+
+  const isLogoNeeded = (rank) => {
+    if (rank < 6 || rank > 17) {
+      return (
+        <div className="flex w-full  h-full">
+          <img
+            src={qualifiedLeague(rank)}
+            className=""
+            alt="cup logo"
+            width="15px"
+            height="15px"
+          />
+        </div>
+      );
+    }
+  };
+
+  const renderTeamName = (teamName) => {
+    if (userWidth >= 1024) {
+      return teamName;
+    } else if (userWidth >= 768) {
+      return getTeamNickname(teamName, userWidth);
+    } else {
+      return getTeamNickname(teamName, userWidth).substr(0, 3);
+    }
+  };
+
   return (
     <div id="standings-child" className="flex text-sm pt-10">
-      <table className="">
-        <thead>
-          <tr className="justify-between grid grid-cols-9 w-full py-5 shadow-md text-xl text-white font-inter bg-slate-400">
-            <th >#</th>
-            <th>Club</th>
-            <th>MP</th>
-            <th>W</th>
-            <th>D</th>
-            <th>L</th>
-            <th>GD</th>
-            <th>Pts</th>
-            <th className="invisible md:visible">Form</th>
-          </tr>
-        </thead>
-        {tableData.map((club) => (
-          <tr
-            key={club.rank}
-            className={isRelegated(club.rank)}>
-            <td>{club.rank}</td>
-            <td className=" flex text-left font-bold ">
-              {getTeamNickname(club.team.name, userWidth)}  
-              {isLogoNeeded(club.rank)}
-              </td>
-            <td className="text-right md:text-center">{club.all.played}</td>
-            <td>{club.all.win}</td>
-            <td >{club.all.draw}</td>
-            <td >{club.all.lose}</td>
-            <td >{club.goalsDiff}</td>
-            <td className="font-bold">{club.points}</td>
-            <td className="text-center invisible md:visible">{club.form}</td>
-          </tr>
-        ))}
-      </table>
+      <div className="overflow-x-auto w-full">
+        <table className="w-full border-collapse">
+          <thead>
+            <tr className="bg-gray-800 text-white">
+              <th className="py-2">#</th>
+              <th className="py-2 text-left">Club</th>
+              <th className="py-2">MP</th>
+              <th className="py-2">W</th>
+              <th className="py-2">D</th>
+              <th className="py-2">L</th>
+              <th className="py-2">GD</th>
+              <th className="py-2">Pts</th>
+              <th className="py-2 text-left hidden md:table-cell">Form</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tableData.map((club) => (
+              <tr key={club.rank} className={isRelegated(club.rank)}>
+                <td className="py-2">{club.rank}</td>
+                <td className="py-2 flex items-center text-left">
+                  <div>{renderTeamName(club.team.name)}</div>
+                  <div className="ml-auto">{isLogoNeeded(club.rank)}</div>
+                </td>
+                <td className="py-2 text-center">{club.all.played}</td>
+                <td className="py-2 text-center">{club.all.win}</td>
+                <td className="py-2 text-center">{club.all.draw}</td>
+                <td className="py-2 text-center">{club.all.lose}</td>
+                <td className="py-2 text-center">{club.goalsDiff}</td>
+                <td className="py-2 font-bold text-center">{club.points}</td>
+                <td className="py-2 font-bold hidden md:table-cell text-left">
+                  {club.form}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
+
 export default Standings;
