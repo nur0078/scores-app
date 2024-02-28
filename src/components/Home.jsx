1bff035169b307e15aa33354f83171a2954c1c97
Anurodh <anurodhp7@gmail.com>
2024-02-28 18:55:47 +1100

    design home layout

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,18 +6,18 @@ const Home = () => {
   return (
     <div className="py-8 font-poppins">
       {/* Main grid layout for desktop and mobile */}
-      <div id="home" className="md:grid md:grid-cols-2 gap-10 justify-center items-center">
+      <div id="home" className="md:grid md:grid-cols-2 justify-center items-center">
         {/* Billboard section */}
         <div id="billboard" className="flex justify-center items-center">
           <Billboard />
         </div>
         {/* Fixtures section */}
-        <div id="fixtures" className="m-2 md:pr-4">
+        <div id="fixtures" className="m-2 pt-8">
           <FixturesList />
         </div>
       </div>
       {/* Standings section */}
-      <div className="flex justify-center items-center mt-10 p-12">
+      <div className="flex justify-center items-center mt-10">
         <Standings />
       </div>
     </div>
