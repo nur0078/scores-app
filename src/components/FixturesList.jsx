5d7130a3c2d02916b677ef8a4bfa2062f277b5f1
Anurodh <anurodhp7@gmail.com>
2024-02-28 17:09:58 +1100

    refactor FixturesList

--- src/components/FixturesList.jsx
+++ src/components/FixturesList.jsx
@@ -1,48 +1,46 @@
 import { useState, useEffect } from "react";
-
-import { fetchNextFixtures, formatDate, formatTime, getTeamNickname } from '../constants'
-
+import { fetchNextFixtures, formatDate, formatTime, getTeamNickname } from '../constants';
 
 const FixturesList = () => {
+  // State to store fixtures data
   const [fixturesData, setFixturesData] = useState([]);
 
   useEffect(() => {
+    // Fetch fixtures data when component mounts
     const fetchData = async () => {
-      const data = await fetchNextFixtures(4);
-      if (data) {
+      try {
+        const data = await fetchNextFixtures(4);
         setFixturesData(data);
+      } catch (error) {
+        console.error("Error fetching fixtures:", error);
       }
     };
     fetchData();
   }, []);
 
-
-
   return (
     <div className="flex">
-      <ul role="list" className="divide-y divide-gray-100 ">
+      {/* Render list of fixtures */}
+      <ul role="list" className="divide-y divide-gray-100">
         {fixturesData.map((game) => (
-          <li
-            key={game.fixture.id}
-            className="justify-between my-3  grid grid-cols-3 items-center shadow-md rounded-lg min-w-fit "
-          >
+          <li key={game.fixture.id} className="flex items-center justify-between my-3 shadow-md rounded-lg">
             {/* Home Team */}
-            <div className="flex items-center ">
+            <div className="flex items-center">
               {/* Home team logo */}
               <img
-                id="homeLogo"
-                className="h-10 w-10 mx-2 rounded-full bg-gray-50 "
+                className="h-10 w-10 mx-2 rounded-full bg-gray-50"
                 src={game.teams.home.logo}
                 alt="home team logo"
               />
               {/* Home team name */}
-              <h2 className="">{getTeamNickname(game.teams.home.name)}</h2>
+              <h2>{getTeamNickname(game.teams.home.name)}</h2>
             </div>
   
-            {/* Game INFO  */}
+            {/* Game INFO */}
             <div className="py-4 mx-6">
               {/* Date */}
-              <div className=" text-lg leading-6 text-gray-600">{formatDate(game.fixture.date)} </div>
+              <div className="text-lg leading-6 text-gray-600">{formatDate(game.fixture.date)}</div>
+              {/* League name */}
               <div className="text-sm font-semibold">{game.league.name}</div>
               {/* Kick-off Time */}
               <p className="text-sm font-semibold leading-6 text-gray-900 py-2">
@@ -53,23 +51,21 @@ const FixturesList = () => {
             </div>
   
             {/* Away TEAM */}
-            <div className="flex items-center justify-end mx-2">
+            <div className="flex items-center">
               {/* Away team name */}
               <h2>{getTeamNickname(game.teams.away.name)}</h2>
               {/* Away team logo */}
               <img
-                id="awayLogo"
-                className="h-10 w-10 flex rounded-full bg-gray-50 mx-1"
+                className="h-10 w-10 mx-2 rounded-full bg-gray-50"
                 src={game.teams.away.logo}
+                alt="away team logo"
               />
             </div>
           </li>
-          
         ))}
       </ul>
     </div>
-    
   );
-        }  
+};
 
 export default FixturesList;
