3471a2ae4008bf7fcd648eeb36a27671f0242181
Anurodh <anurodhp7@gmail.com>
2024-02-28 15:20:46 +1100

    set and design LiveStatus, fixture details for billboard

--- src/components/Billboard.jsx
+++ src/components/Billboard.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
-import { fetchLastFixtures, formatDate, liveFixtures } from "../constants";
+import { fetchLastFixtures, liveFixtures } from "../constants";
 
 const Billboard = () => {
   const [boardScore, setBoardScore] = useState([]);
+  const [isLive, setIsLive] = useState(false);
 
   useEffect(() => {
     const fetchLive = async () => {
@@ -10,6 +11,7 @@ const Billboard = () => {
         const liveData = await liveFixtures();
         if (liveData.length > 0) {
           setBoardScore(liveData);
+          setIsLive(true);
         } else {
           const lastGameData = await fetchLastFixtures(1);
           setBoardScore(lastGameData);
@@ -24,37 +26,36 @@ const Billboard = () => {
   console.log("boardScore", boardScore);
 
   return (
-    <div className="rounded-3xl bg-pale-blue shadow-2xl w-[400px] h-[400px] lg:h-[450px] border-2 border-yellow-500">
+    <div className="rounded-3xl bg-pale-blue shadow-2xl p w-[400px] h-[400px] lg:h-[450px]">
       {boardScore.length > 0 && (
         <div className="justify-center items-center">
           {/*============ Top Bins ============ */}
           <div className="flex-col text-left py-4">
             {/* =========== League and Week Info =========== */}
-            <div id="leagueName" className="">
-              <div className="flex justify-center border-2 border-red-500 items-center">
-                <span className="flex justify-around mx-4 w-full ">
-                  <img
-                    src={boardScore[0].league.logo}
-                    alt="League logo"
-                    className="w-[60px] "
-                  />
-                  <div className="border-2 flex-1 text-center border-blue-500 text-md my-4">
-                    {boardScore[0].league.name}
-                  </div>
-                </span>
-                <span className="text-right mx-4">Live</span>
-              </div>
-              {/* ========== GameWeek ========== */}
-              <small className="ml-4 text-sm">
-                Gameweek {boardScore[0].league.round.slice(-2)}
-              </small>
+            <div className="flex justify-between items-center ">
+              <span className="flex items-center">
+                <img
+                  src={boardScore[0].league.logo}
+                  alt="League logo"
+                  className="w-[30px] ml-4"
+                />
+                <div className="flex items-center text-md font-semibold ml-2">
+                  {boardScore[0].league.name}
+                </div>
+              </span>
+              <span
+                id="LiveText"
+                className="border-2 border-red-400 whitespace-no-wrap mr-4"
+              >
+                {isLive ? "Live" : "GW " + boardScore[0].league.round.slice(-2)}
+              </span>
             </div>
           </div>
 
           {/* SCOREBOARD */}
-          <div className="border-2 border-purple-500 flex items-center justify-between">
+          <div className="flex items-center justify-between pt-6">
             {/* HOME TEAM */}
-            <div className="border-2 border-blue-500 flex justify-center">
+            <div className="flex justify-center">
               <img
                 src={boardScore[0].teams.home.logo}
                 alt="home team logo"
@@ -64,16 +65,20 @@ const Billboard = () => {
             {/* SCORES */}
             <div className="flex items-center gap-4">
               {/* HOME GOALS */}
-              <div className="text-4xl font-bold">{boardScore[0].goals.home}</div>
+              <div className="text-4xl font-bold">
+                {boardScore[0].goals.home}
+              </div>
               <div className="flex-none flex items-center justify-center h-8 w-8 bg-gray-300 rounded-full">
                 vs
               </div>
               {/* AWAY GOALS */}
-              <div className="text-4xl font-bold">{boardScore[0].goals.away}</div>
+              <div className="text-4xl font-bold">
+                {boardScore[0].goals.away}
+              </div>
             </div>
 
             {/* AWAY TEAM */}
-            <div className="border-2 border-blue-500 flex justify-center">
+            <div className="flex justify-center">
               <img
                 src={boardScore[0].teams.away.logo}
                 alt="away team logo"
@@ -83,19 +88,28 @@ const Billboard = () => {
           </div>
 
           {/* Game Time */}
-          <div>
-            {boardScore[0].fixture && (
-              boardScore[0].fixture.status.short
+          <div className="font-bold">
+            {boardScore[0].fixture &&
+              (boardScore[0].fixture.status.short
                 ? boardScore[0].fixture.status.short
-                : (boardScore[0].fixture.status.elapsed)
-            )}
+                : boardScore[0].fixture.status.elapsed)}
+            <br />
+            <span className="font-medium">
+              {isLive ? "GW " + boardScore[0].league.round.slice(-2) : ""}
+            </span>
           </div>
 
           {/* Lower Text, Team Name Stadium */}
-          <div className="text-center py-3 flex gap-5 border-2 border-red-500 ">
-            <div className="flex-1 font-bold">{boardScore[0].teams.home.name}</div>
-            <div className="text-sm leading-5 text-gray-600">{boardScore[0].fixture.venue.name}</div>
-            <div className="flex-1 font-bold">{boardScore[0].teams.away.name}</div>
+          <div className="text-center py-3 flex gap-5">
+            <div className="flex-1 font-bold">
+              {boardScore[0].teams.home.name}
+            </div>
+            <div className="text-sm leading-5 text-gray-600">
+              {boardScore[0].fixture.venue.name}
+            </div>
+            <div className="flex-1 font-bold">
+              {boardScore[0].teams.away.name}
+            </div>
           </div>
         </div>
       )}
