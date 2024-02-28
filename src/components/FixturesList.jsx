dd3ec670af2c8150eabb13588b15bb8f36d08050
Anurodh <anurodhp7@gmail.com>
2024-02-28 14:51:21 +1100

    fetch api data from constant

--- src/components/FixturesList.jsx
+++ src/components/FixturesList.jsx
@@ -1,49 +1,22 @@
 import { useState, useEffect } from "react";
-import axios from 'axios';
-import { getTeamNickname } from '../constants'
+
+import { fetchNextFixtures, formatDate, formatTime, getTeamNickname } from '../constants'
 
 
 const FixturesList = () => {
   const [fixturesData, setFixturesData] = useState([]);
 
   useEffect(() => {
-    const fetchFixtures = async () => {
-      try {
-        const response = await axios.get('https://api-football-v1.p.rapidapi.com/v3/fixtures', {
-          params: {
-            league: '39',
-            team: '33',
-            next: '5',
-            timezone: 'Australia/Sydney'
-          },
-          headers: {
-            'X-RapidAPI-Key': import.meta.env.VITE_REACT_APP_RAPIDAPI_KEY,
-            'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
-          }
-        });
-        setFixturesData(response.data.response);
-      } catch (error) {
-        console.error(error);
+    const fetchData = async () => {
+      const data = await fetchNextFixtures(4);
+      if (data) {
+        setFixturesData(data);
       }
     };
-    fetchFixtures();
+    fetchData();
   }, []);
 
-  // Format for date
-  const formatDate = (dateString) => {
-    const dateTime = new Date(dateString);
-    const month = dateTime.toLocaleString('default', { month: 'short' });
-    const day = dateTime.getDate().toString().padStart(2, '0');
-    return `${month} ${day}`;
-  };
 
-  // Function to format time to 24-hour format
-  const formatTime = (timeString) => {
-    const dateTime = new Date(timeString);
-    const hours = dateTime.getHours().toString().padStart(2, '0');
-    const minutes = dateTime.getMinutes().toString().padStart(2, '0');
-    return `${hours}:${minutes}`;
-  };
 
   return (
     <div className="flex">
@@ -69,7 +42,8 @@ const FixturesList = () => {
             {/* Game INFO  */}
             <div className="py-4 mx-6">
               {/* Date */}
-              <div className=" text-lg leading-6 text-gray-600">{formatDate(game.fixture.date)}</div>
+              <div className=" text-lg leading-6 text-gray-600">{formatDate(game.fixture.date)} </div>
+              <div className="text-sm font-semibold">{game.league.name}</div>
               {/* Kick-off Time */}
               <p className="text-sm font-semibold leading-6 text-gray-900 py-2">
                 {formatTime(game.fixture.date)}
