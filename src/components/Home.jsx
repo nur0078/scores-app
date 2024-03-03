e56fa6b7926353ad97ce9ef6ebd9d37ac616364d
Anurodh <anurodhp7@gmail.com>
2024-03-04 06:01:21 +1100

    add Nav to home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,32 @@
 import Billboard from "./Billboard";
 import Standings from "./Standings";
 import FixturesList from "./FixturesList";
+import Nav from "./Nav";
 
 // Home component renders the main dashboard layout
 const Home = () => {
   return (
     <div className="py-8 font-poppins">
       {/* Main grid layout for desktop and mobile */}
-      <div id="home" className="md:grid md:grid-cols-2 justify-center items-center">
-        {/* Billboard section */}
-        <div id="billboard" className="flex justify-center items-center">
-          <Billboard />
-        </div>
-        {/* Fixtures section */}
-        <div id="fixtures" className="m-2 pt-8">
-          <FixturesList />
+      <div className="h-screen bg-pale-blue">
+        {/* NAVBAR */}
+        <Nav />
+        <div
+          id="home"
+          className=" md:grid md:grid-cols-2 justify-center items-center"
+        >
+          {/* Billboard section */}
+          <div id="billboard" className="flex justify-center items-center">
+            <Billboard />
+          </div>
+          {/* Fixtures section */}
+          <div id="fixtures" className="m-2 pt-8">
+            <FixturesList />
+          </div>
         </div>
       </div>
       {/* Standings section */}
-      <div className="flex justify-center items-center mt-10">
+      <div className="flex justify-center items-center mt-10 h-screen">
         <Standings />
       </div>
     </div>
