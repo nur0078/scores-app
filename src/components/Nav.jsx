8caa0aee4115ac8528bea95681c10f6a4d804ae2
Anurodh <anurodhp7@gmail.com>
2024-03-04 06:01:00 +1100

    add text and style to nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,15 +1,14 @@
-import footballLogo from '../assets/logo/footballLogo.svg'
+import footballLogo from "../assets/logo/footballLogo.svg";
 
 const Nav = () => {
   return (
-    <div className='flex text-2xl text-center justify-center'>
-      <img 
-      src={footballLogo}
-      alt="football logo"
-      className='h-8 w-8'
-      />
+    <div className="flex text-2xl text-center justify-center mb-6 underline hover:no-underline">
+      {/* TITLE */}
+      <h1 className="text-4xl "> scores </h1>
+      {/* FootballLogo */}
+      <img src={footballLogo} alt="football logo" className="h-8 w-8 " />
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
