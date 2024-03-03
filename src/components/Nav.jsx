46e83d8b963c7e967b224a6bd6c568e864c3e7f1
Anurodh <anurodhp7@gmail.com>
2024-03-04 05:49:46 +1100

    add Nav elements

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,14 @@
+import footballLogo from '../assets/logo/footballLogo.svg'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <div className='flex text-2xl text-center justify-center'>
+      <img 
+      src={footballLogo}
+      alt="football logo"
+      className='h-8 w-8'
+      />
+    </div>
   )
 }
 
