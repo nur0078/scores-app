4a941a85c9728ba858c15b2667bc0c2feba47dfa
Anurodh <anurodhp7@gmail.com>
2024-03-04 18:40:17 +1100

    fix div style error in billboard and fixtureList

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,25 +6,25 @@ import Nav from "./Nav";
 // Home component renders the main dashboard layout
 const Home = () => {
   return (
-    <div className="py-8 font-poppins">
+    <div className="py-8 font-poppins p-8">
       {/* Main grid layout for desktop and mobile */}
-      <div className="h-screen ">
-        {/* NAVBAR */}
-        <Nav />
-        <div
-          id="home"
-          className=" md:grid md:grid-cols-2 justify-center items-center"
-        >
-          {/* Billboard section */}
-          <div id="billboard" className="flex justify-center items-center">
-            <Billboard />
-          </div>
-          {/* Fixtures section */}
-          <div id="fixtures" className="m-2 pt-8">
-            <FixturesList />
-          </div>
+      {/* <div className="h-screen "> */}
+      {/* NAVBAR */}
+      <Nav />
+      <div
+        id="home"
+        className=" md:grid md:grid-cols-2 justify-center items-center"
+      >
+        {/* Billboard section */}
+        <div id="billboard" className="flex justify-center items-center">
+          <Billboard />
+        </div>
+        {/* Fixtures section */}
+        <div id="fixtures" className="m-2 ">
+          <FixturesList />
         </div>
       </div>
+      {/* </div> */}
       {/* Standings section */}
       <div className="flex justify-center items-center mt-10 h-screen">
         <Standings />
